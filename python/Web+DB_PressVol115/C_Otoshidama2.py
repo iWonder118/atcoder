c8e9b034434f3f929c19f7c283e66f111c8edab3
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-03-06 17:06:53 +0900

    C_Otoshidama2 ac

---
+++ python/Web+DB_PressVol115/C_Otoshidama2.py
@@ -0,0 +1,11 @@
+import sys
+
+n, y = map(int, input().split())
+for i in range(n+1):
+  for j in range(min(n-i+1, 9)):
+    k = n - i - j
+    money = 10000 * i + 5000 * j + 1000 * k
+    if y == money:
+      print("{} {} {}".format(i, j, k))
+      sys.exit()
+print(-1, -1, -1)
