8ac8255e70e85301f7ae2c9d19562cff56a11525
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-03-30 22:35:04 +0900

    ABC147 B AC

--- python/ABC147/B.py
+++ python/ABC147/B.py
@@ -2,11 +2,11 @@ s = list(input())
 s_len = len(s)
 ans = 0
 if s_len % 2 == 0:
-  for i, j in zip(s[0 : (s_len // 2 -1)], reversed(s[(s_len // 2) : s_len])):
+  for i, j in zip(s[0 : (s_len // 2 )], reversed(s[(s_len // 2) : s_len + 1])):
     if i != j:
       ans += 1
 else:
-  for i, j in zip(s[0 : (s_len // 2 -1)], reversed(s[(s_len // 2 + 2) : s_len])):
+  for i, j in zip(s[0 : (s_len // 2  )], reversed(s[(s_len // 2 + 1) : s_len])):
     if i != j:
       ans += 1
 print(ans)
