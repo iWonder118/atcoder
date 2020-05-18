ac6b80d996f2b004664d09037398973ce77e242b
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-05-18 22:25:57 +0900

    AGC014 A ac

--- python/AGC014/A.py
+++ python/AGC014/A.py
@@ -1,10 +1,23 @@
 def main():
 	A, B, C = map(int, input().split())
 	even_flag = True
+	count = 0
+
 	while even_flag:
 		if A % 2 == 1 or B % 2 == 1 or C % 2 == 1:
 			even_flag = False
+			break
 
-		A, B, C == B // 2
-if __name__ == "main":
+		elif A == B and B == C and A == C:
+			break
+
+		A, B, C = (B // 2 + C // 2), (A // 2 + C // 2), (A // 2 + B // 2)
+		count += 1
+
+	if even_flag:
+		print(-1)
+	else:
+		print(count)
+		
+if __name__ == "__main__":
 	main()
