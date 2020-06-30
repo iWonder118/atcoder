38b9f08567597df71f112afbfb3fd20514035bc2
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-06-30 12:54:52 +0900

    Easy 100 40]

---
+++ python/AGC002/A.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+if 0 < a <= b:
+    print("Positive")
+
+elif a <= b < 0:
+    if (b - a + 1) % 2 == 0:
+        print("Positive")
+
+    else:
+        print("Negative")
+elif a <= 0 <= b:
+    print("Zero")
