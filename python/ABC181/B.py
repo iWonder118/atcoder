95fc6806d3ad9096eebc544268402c979292fc7d
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-11-02 12:24:38 +0900

    ABC181 A ac B tle c

---
+++ python/ABC181/B.py
@@ -0,0 +1,11 @@
+from itertools import accumulate
+
+n = int(input())
+boad = [list(map(int, input().split())) for _ in range(n)]
+ans = []
+for low, high in boad:
+    ans.append()
+total = 0
+for out in ans:
+    total += sum(out)
+print(total)
