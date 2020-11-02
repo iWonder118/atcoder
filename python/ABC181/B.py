bda6d5a86c3fed9acb249794c4ee07808f16339c
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-11-02 13:03:04 +0900

    ABC181　B-C acc

--- python/ABC181/B.py
+++ python/ABC181/B.py
@@ -1,11 +1,8 @@
-from itertools import accumulate
-
 n = int(input())
 boad = [list(map(int, input().split())) for _ in range(n)]
 ans = []
 for low, high in boad:
-    ans.append()
-total = 0
-for out in ans:
-    total += sum(out)
-print(total)
+    plus_low = low * (low - 1) // 2
+    plus_high = high * (high + 1) // 2
+    ans.append(plus_high - plus_low)
+print(sum(ans))
