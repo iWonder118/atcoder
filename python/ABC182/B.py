0f97d08c107f892b371750974fb85a9dd5aceaa3
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-11-12 13:16:12 +0900

    ABC182 B ac

---
+++ python/ABC182/B.py
@@ -0,0 +1,12 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+ans = -1
+mx = 0
+
+for x in range(2, 1001):
+    s = sum(a % x == 0 for a in numbers)
+    if mx < s:
+        mx = s
+        ans = x
+
+print(ans)
