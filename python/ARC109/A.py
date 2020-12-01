046b409fd7125d4d9dc85b3d301b4382a555ac09
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-12-01 12:48:21 +0900

    ARC109 A ac

--- python/ARC109/A.py
+++ python/ARC109/A.py
@@ -1,21 +1,27 @@
-def make_divisors(n):
-    lower_divisors, upper_divisors = [], []
-    i = 1
-    while i * i <= n:
-        if n % i == 0:
-            lower_divisors.append(i)
-            if i != n // i:
-                upper_divisors.append(n // i)
-        i += 1
-    return lower_divisors + upper_divisors[::-1]
+# def make_divisors(n):
+#     lower_divisors, upper_divisors = [], []
+#     i = 1
+#     while i * i <= n:
+#         if n % i == 0:
+#             lower_divisors.append(i)
+#             if i != n // i:
+#                 upper_divisors.append(n // i)
+#         i += 1
+#     return lower_divisors + upper_divisors[::-1]
+# max_num_divisor = make_divisors(max(s, p))
+# for divisor in max_num_divisor:
+#     plus = min(s, p) + divisor
+#     multi = max(s, p) * divisor
+#     if plus == multi:
+#         print("Yes")
+#         exit()
+import math
 
 
 s, p = map(int, input().split())
-max_num_divisor = make_divisors(max(s, p))
-for divisor in max_num_divisor:
-    plus = min(s, p) + divisor
-    multi = max(s, p) * divisor
-    if plus == multi:
+max_range = math.sqrt(10 ** 13)
+for i in range(int(max_range)):
+    if i * (s - i) == p:
         print("Yes")
         exit()
 print("No")
