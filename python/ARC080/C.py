fb4d83881e9d08ccc1733681f94dc977e9d6facd
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-12-15 13:05:37 +0900

    Medium 100 31 ac

--- python/ARC080/C.py
+++ python/ARC080/C.py
@@ -1,18 +1,18 @@
 n = int(input())
 numbers = list(map(int, input().split()))
-multiple_0_time = []
-multiple_1_time = []
-multiple_over_time = []
+multiple_4 = []
+multiple_2 = []
+other = []
 for i in range(n):
-    if numbers[i] // 2 == 0:
-        multiple_0_time.append(numbers[i])
-    if numbers[i] // 2 == 2:
-        multiple_1_time.append(numbers[i])
+    if numbers[i] % 4 == 0:
+        multiple_4.append(numbers[i])
+    elif numbers[i] % 2 == 0:
+        multiple_2.append(numbers[i])
     else:
-        multiple_over_time.append(numbers[i])
-if len(multiple_1_time) > 0 and len(multiple_0_time) == len(multiple_over_time) + 1:
+        other.append(numbers[i])
+if len(multiple_2) == 0 and len(other) <= len(multiple_4) + 1:
     print("Yes")
-elif not (len(multiple_1_time) > 0) and len(multiple_0_time) == len(multiple_over_time):
+elif len(multiple_2) != 0 and len(other) <= len(multiple_4):
     print("Yes")
 else:
     print("No")
