518f78d1f6c6fded3bebd2e4655b338e8a36b3de
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-12-23 12:24:56 +0900

    ABC186 C ac

--- python/ABC186/C.py
+++ python/ABC186/C.py
@@ -1,23 +1,6 @@
-def mod(n):
-    num = n // 8
-    mod = n % 8
-    tmp = []
-    if num == 0:
-        return "".join(reversed(tmp))
-    else:
-        tmp.append(str(mod))
-        return mod(num)
-
-
-def main():
-    n = int(input())
-    ans = n
-    for i in range(1, n + 1):
-        print(mod(i))
-        if "7" in str(i) or "7" in mod(i):
-            ans -= 1
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
+n = int(input())
+ans = n
+for i in range(1, n + 1):
+    if "7" in str(i) or "7" in oct(i):
+        ans -= 1
+print(ans)
