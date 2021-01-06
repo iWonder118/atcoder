6984bd4b360d7e900fb0b72a3ac49c6203c5f792
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-01-06 12:35:40 +0900

    Meduim 100 37

--- python/ABC116/C.py
+++ python/ABC116/C.py
@@ -1,14 +1,16 @@
 n = int(input())
 flowers = list(map(int, input().split()))
-if flowers.count(0) == 0:
-    ans = 1
-else:
-    ans = 1 + flowers.count(0)
-while True:
-    flowers = list(map(lambda x: max(-1, x - 1), flowers))
-    flowers = list(map(lambda x: -1 if x == 0 else x, flowers))
-    tmp = "".join(map(str, flowers)).split("-1")
-    if flowers.count(-1) == n:
-        break
-    ans += len(tmp) - tmp.count("")
+ans = 0
+while flowers.count(0) != n:
+    count = False
+    for i in range(n):
+        if flowers[i] > 0:
+            flowers[i] -= 1
+            count = True
+        elif flowers[i] == 0 and count:
+            ans += 1
+            count = False
+    if count:
+        ans += 1
+
 print(ans)
