f82fe78e0df44afd799d7e2b13ebd7f7888c02f7
iWonder118 <iwonder16118@gmail.com>
2021-02-22 03:22:58 +0000

    ABC192 C ac

--- python/ABC191/C.py
+++ python/ABC191/C.py
@@ -1,17 +1,16 @@
 h, w = map(int, input().split())
 graph = [list(input()) for _ in range(h)]
 ans = 0
-for i in range(h):
-    for j in range(w):
-        if graph[i][j] is "#":
-            around = []
-            count = 0
-            for xi in range(-1, 2):
-                for yj in range(-1, 2):
-                    around.append([max(0, min(i + xi, w)), max(0, min(j + yj, h))])
-            for point in around:
-                if graph[point[0]][point[1]] is "#":
-                    count += 1
-            if count == 4:
-                ans += 1
+for i in range(h - 1):
+    for j in range(w - 1):
+        around = []
+        count = 0
+        for xi in range(2):
+            for yj in range(2):
+                around.append([i + xi, j + yj])
+        for point in around:
+            if graph[point[0]][point[1]] is "#":
+                count += 1
+        if count == 1 or count == 3:
+            ans += 1
 print(ans)
