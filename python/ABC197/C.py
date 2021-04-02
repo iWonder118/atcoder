05584a10e9e58ff347e1349d4b5e8251a8c1a3c1
iWonder118 <iwonder16118@gmail.com>
2021-04-02 03:52:29 +0000

    ABC197 C anser

--- python/ABC197/C.py
+++ python/ABC197/C.py
@@ -1,12 +1,16 @@
 n = int(input())
 numbers = list(map(int, input().split()))
-ans = []
-for i in range(1, n):
-    temp1 = 0
-    temp2 = 0
-    for number in numbers[:i]:
-        temp1 |= number
-    for number in numbers[i:]:
-        temp2 |= number
-    ans.append(temp1 ^ temp2)
-print(min(ans))
+ans = 2 ** 30
+for i in range(1 << (n - 1)):
+    now = 0
+    temp = 0
+    for j in range(n):
+        temp |= numbers[j]
+        
+        if i >> j & 1:
+            now ^= temp
+            temp - 0
+    now ^= temp
+    ans = min(ans, now)
+    
+print(ans)
