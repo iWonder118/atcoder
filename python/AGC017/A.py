41421af87f031beadf34a64297da292bb10cebe4
iWonder118 <iwonder16118@gmail.com>
2021-04-22 04:01:46 +0000

    Medium 100 47 ac

--- python/AGC017/A.py
+++ python/AGC017/A.py
@@ -1,13 +1,12 @@
 n, p = map(int, input().split())
 bisckets = list(map(int, input().split()))
-ans = 0
-for i in range(2 ** n):
-    eat = []
-    total = 0
-    for j in range(n):
-        if ((i >> j) & 1):
-            eat.append(bisckets[j])
-            total += bisckets[j]
-    if (total % 2 == p):
-        ans += 1
-print(ans)
+contain_odd = False
+for biscket in bisckets:
+    if biscket % 2 == 1:
+        contain_odd = True
+if not contain_odd and p == 0:
+    print(2 ** n)
+elif not contain_odd and p == 1:
+    print(0)
+else:
+    print(2 ** (n - 1))
