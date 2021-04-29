d7544f623fcf421340a7dacc9345091360802493
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-04-29 23:39:28 +0900

    Medium 100 19 ac

--- python/diverta2019/B.py
+++ python/diverta2019/B.py
@@ -1,7 +1,9 @@
-r, g, b, n = map(int, input().split())
+r, g, b, n = map(int,input().split())
 ans = 0
-for i in range(n // r + 1):
-    for j in range((n - i * r) // g + 1):
-        if (n - i * r - j * g) % b == 0:
+for i in range(3001):
+    for j in range(3001):
+        if (n-r*i-g*j) < 0:
+            continue
+        if (n - r * i - g * j) % b == 0:
             ans += 1
 print(ans)
