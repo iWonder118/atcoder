33e449b76b10298b6f98ad665f3bca4aa2bc7b32
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-05-19 12:49:53 +0900

    Revert "mend"
    This reverts commit 01076c18427f3e4b13c60b1cd9ccc93636497013.

--- python/ABC199/C.py
+++ python/ABC199/C.py
@@ -2,19 +2,15 @@ n = int(input())
 s = list(input())
 q = int(input())
 numbers = [list(map(int, input().split())) for _ in range(q)]
-isReverse = True
+
 for i in range(q):
     if numbers[i][0] == 1:
-        temp = s[numbers[i][1] - 1]
-        s[numbers[i][1] - 1] = s[numbers[i][2] - 1]
-        s[numbers[i][2] - 1] = temp
+        temp = s[numbers[i][1]]
+        s[numbers[i][1]] = s[numbers[i][2]]
+        s[numbers[i][2]] = temp
     else:
-        if isReverse:
-            isReverse = False
-        else:
-            isReverse = True
-ans = "".join(s)
-if isReverse:
-    print(ans[n:] + ans[:n])
-else:
-    print(ans)
+        str_s = "".join(s)
+        print(str_s)
+        s = list(str_s[n:] + str_s[:n + 1])
+
+print("".join(s))
