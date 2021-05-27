862fbb6c4daa22874128d83051ee8e288adef3ff
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-05-28 00:00:10 +0900

    ABC194 B

--- python/ABC194/B.py
+++ python/ABC194/B.py
@@ -1,14 +1,16 @@
-n = int(input())
-empolyee = [list(map(int, input().split())) for _ in range(n)]
-empolyee_dict = []
-for i in range(n):
-    empolyee_dict.append({"a": empolyee[i][0], "b": empolyee[i][1], "total": empolyee[i][0] + empolyee[i][1]})
-empolyee_sorted_total = sorted(empolyee_dict, key=lambda x: x['total'])
-empolyee_sorted_a = sorted(empolyee_dict, key=lambda x: x['a'])
-empolyee_sorted_b = sorted(empolyee_dict, key=lambda x: x['b'])
-print(empolyee_sorted_total)
-print(empolyee_sorted_a)
-print(empolyee_sorted_b)
-pattrn1 = max(empolyee_sorted_a[0]["a"], empolyee_sorted_b[1]["b"])
-pattrn2 = max(empolyee_sorted_a[1]["a"], empolyee_sorted_b[0]["b"])
-print(min(min(pattrn1, pattrn2), empolyee_sorted_total[0]["total"]))
+from collections import Counter
+
+N = int(input())
+A = list(map(int, input().split()))
+cnt = Counter(A)
+
+ans = 0
+
+for x in range(-200, 200 + 1):
+    for y in range(x + 1, 200 + 1):
+        xcnt = cnt[x]
+        ycnt = cnt[y]
+        total = xcnt * ycnt * (x - y) ** 2
+        ans += total
+
+print(ans)
