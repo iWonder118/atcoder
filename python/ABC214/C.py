f881a8dea8b374663580a99a1c46296dc08ee8b8
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-08-16 23:50:09 +0900

    ABC214 C ac

--- python/ABC214/C.py
+++ python/ABC214/C.py
@@ -1,11 +1,10 @@
 n = int(input())
 s = list(map(int, input().split()))
 t = list(map(int, input().split()))
-INF = 10 ** 9 + 1
-dp = [INF] * (n + 1)
-dp[0] = t[0]
-for i in range(n - 1):
-    dp[i + 1] = min(dp[i] + s[i], t[i + 1])
 
-for i in range(n):
-    print(dp[i])
+for i in range(2 * n):
+    t[i % n] = min(t[i % n], t[(i - 1) % n] + s[(i - 1) % n])
+ 
+for ans in t:
+    print(ans)
+    
